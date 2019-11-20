4d9de00991c257618fce187aa2dd4b4ba3505993
Freddy <kueen@protonmail.com>
2019-11-20 08:40:56 +0200

    Fix default cron expression

--- PCloud.Backup/BackupConfig.cs
+++ PCloud.Backup/BackupConfig.cs
@@ -5,7 +5,7 @@
     public string PCloudCode { get; set; }
     public string BackupFolder { get; } = "data";
     public string BackupLabel { get; set; } = "docker";
-    public string BackupCronExpression { get; set; } = "0 0 0 0/1 * ?";
+    public string BackupCronExpression { get; set; } = "0 0 0 * * ?";
   }
 }
 
